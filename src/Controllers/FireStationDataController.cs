86e358926ce5e936277804fac829768803b1a4a5
coreyan <coreyan@vip.qq.com>
2018-03-12 10:15:07 +0800

    mem

--- src/Controllers/FireStationDataController.cs
+++ src/Controllers/FireStationDataController.cs
@@ -37,10 +37,10 @@ namespace Preoff.Controllers
         /// <summary>
         /// 根据日期和时间获取火险等级
         /// </summary>
-        /// <param name="date">日期20180309</param>
-        /// <param name="hour">小时08</param>
+        /// <param name="date">日期(20180309)</param>
+        /// <param name="hour">小时(08)</param>
         /// <returns></returns>
-        [HttpPost("GetList")]
+        [HttpGet("GetList")]
         public IActionResult SelectPage(string date,string hour)
         {
             if (date.Length!=8)
